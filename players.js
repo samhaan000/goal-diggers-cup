426d40321ee5d738382d1480fc57bccf070d5f21
samhaan000 <samhaan000@gmail.com>
2026-05-27 03:50:41 +0500

    Add player database

---
+++ players.js
@@ -0,0 +1,51 @@
+window.goalDiggersPlayers={
+  "Meem Police":[
+    {id:"meem_police_8",number:8,name:"Imjad Ahmed",team:"Meem Police"},
+    {id:"meem_police_7",number:7,name:"Mohamed Shaahu",team:"Meem Police"},
+    {id:"meem_police_2",number:2,name:"Ali Razzan",team:"Meem Police"},
+    {id:"meem_police_10",number:10,name:"Abdulla Amjadh",team:"Meem Police"},
+    {id:"meem_police_5",number:5,name:"Ahmed Rafhan Mohamed",team:"Meem Police"}
+  ],
+  "FC Karaa":[
+    {id:"fc_karaa_17",number:17,name:"Ali Ziyad",team:"FC Karaa"},
+    {id:"fc_karaa_12",number:12,name:"Ahmed Rasheed",team:"FC Karaa"},
+    {id:"fc_karaa_21",number:21,name:"Ali Ashraf",team:"FC Karaa"},
+    {id:"fc_karaa_7",number:7,name:"Hassan Ashraf",team:"FC Karaa"},
+    {id:"fc_karaa_25",number:25,name:"Ahmed Shahid",team:"FC Karaa"}
+  ],
+  "Triple T":[
+    {id:"triple_t_21",number:21,name:"Mohamed Alim Ahmed",team:"Triple T"},
+    {id:"triple_t_7",number:7,name:"Hussain Mizdhaan Ahmed",team:"Triple T"},
+    {id:"triple_t_11",number:11,name:"Ali Ishaan Hassan",team:"Triple T"},
+    {id:"triple_t_9",number:9,name:"Mohamed Ausham",team:"Triple T"},
+    {id:"triple_t_96",number:96,name:"Ahmed Akmal Mohamed",team:"Triple T"}
+  ],
+  "Muli Blues":[
+    {id:"muli_blues_13",number:13,name:"Ahmed Tholaal",team:"Muli Blues"},
+    {id:"muli_blues_4",number:4,name:"Hannadh Ibrahim",team:"Muli Blues"},
+    {id:"muli_blues_16",number:16,name:"Ahmed Sobree",team:"Muli Blues"},
+    {id:"muli_blues_28",number:28,name:"Mifraah Mukhuthaar",team:"Muli Blues"},
+    {id:"muli_blues_22",number:22,name:"Hassan Azumeen",team:"Muli Blues"}
+  ],
+  "Smashers FC":[
+    {id:"smashers_fc_96",number:96,name:"Mohamed Fazeel",team:"Smashers FC"},
+    {id:"smashers_fc_3",number:3,name:"Abdulla Nafiu",team:"Smashers FC"},
+    {id:"smashers_fc_1",number:1,name:"Mohamed Yazdhaan Ahmed",team:"Smashers FC"},
+    {id:"smashers_fc_9",number:9,name:"Misbah Abdulla Waheed",team:"Smashers FC"},
+    {id:"smashers_fc_99",number:99,name:"Ahmed Waheed",team:"Smashers FC"}
+  ],
+  "Blackout 5":[
+    {id:"blackout_5_10",number:10,name:"Aiham Adnan",team:"Blackout 5"},
+    {id:"blackout_5_17",number:17,name:"Mohamed Afnaan",team:"Blackout 5"},
+    {id:"blackout_5_7",number:7,name:"Abdulla Amhal Ashhad",team:"Blackout 5"},
+    {id:"blackout_5_12",number:12,name:"Mohamed Yaaseen Khaleel",team:"Blackout 5"},
+    {id:"blackout_5_19",number:19,name:"Aiham Ahmed Shiyam",team:"Blackout 5"}
+  ],
+  "FC Maavadibe":[
+    {id:"fc_maavadibe_40",number:40,name:"Rishad Mohamed",team:"FC Maavadibe"},
+    {id:"fc_maavadibe_7",number:7,name:"Ahmed Areef",team:"FC Maavadibe"},
+    {id:"fc_maavadibe_19",number:19,name:"Saafy Mohamed",team:"FC Maavadibe"},
+    {id:"fc_maavadibe_4",number:4,name:"Mohamed Rizan Ali",team:"FC Maavadibe"},
+    {id:"fc_maavadibe_41",number:41,name:"Ali Riza",team:"FC Maavadibe"}
+  ]
+};
